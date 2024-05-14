a1bbe9d759376c039628449c6e819626d81c99a1
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2024-05-14 13:36:22 +0200

    change scoring setup

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,7 +4,7 @@ sys.path.append("../utils")
 import preproc as pc
 
 #source location
-in_path = "/net/meso/climphys/cesm212/" #if it ever changes back to climphys and not climphys1, you have to also change preproc.py
+in_path = "/net/meso/climphys/cesm212/" 
 # processed data
 output_path = '/net/xenon/climphys/lbloin/optim_boost/'
 # boost data
@@ -13,12 +13,10 @@ boost_path = "/net/meso/climphys/cesm212/boosting/archive/"
 # configurations
 try: 
     areas = [sys.argv[1]]  # string of area to preprocess
-    clim = eval(sys.argv[2]) # whether to preproc this (either true or false)
-    unpert = eval(sys.argv[3]) # whether to preproc this (either true or false)
-    boost = eval(sys.argv[4]) # whether to preproc this (either true or false)
+    unpert = eval(sys.argv[2]) # whether to preproc this (either true or false)
+    boost = eval(sys.argv[3]) # whether to preproc this (either true or false)
 except IndexError:
     areas = ["PNW","CH","MID","PAR","global"]
-    clim = True
     unpert = True
     boost = True
 print(areas)
@@ -32,9 +30,6 @@ for area in areas:
     else:
         read_type = "box"
     print(read_type)
-    if clim == True:
-        print("preprocessing climatology")
-        pc.preproc_clim(in_path, output_path,area,read_type)
     if unpert == True:
         print("preprocessing unperturbed runs")
         pc.preproc_unpert(in_path, output_path,area,read_type)
